a154409d007e99c66564422b5f57de7514d457c9
Adrian Calancea <adrian.calancea95@gmail.com>
2017-07-12 11:47:29 +0300

    2 th commit

--- src/Constructors/Om.java
+++ src/Constructors/Om.java
@@ -9,4 +9,6 @@ public class Om{
     public Om(){System.out.println("A fost apelat constructorul de baza");}
 
    public void ShowSomething(){System.out.println("THIS is AN OM");}
+
+   public void ihaveditedSomething(){}
 }
